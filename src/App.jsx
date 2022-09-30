3e7a6413456b25936f163d2b5d5a0ac12e1b0d97
Durant Schoon <durant.schoon@gmail.com>
2022-09-30 09:42:05 -0400

    clean up IconButton and MenuIcon comments from App

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,6 @@ export default function App() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          {/* <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton> */}
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Noun-ify!
           </Typography>
