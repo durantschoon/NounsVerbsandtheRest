a68d589cc0114f7c88e96f79e05c260dca3a7cf1
Durant Schoon <durant.schoon@gmail.com>
2022-10-11 16:51:11 -0400

    custom color theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,40 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 // import IconButton from '@mui/material/IconButton';
 // import MenuIcon from '@mui/icons-material/Menu';
 
 import './App.css'
 import InputText from './components/InputText'
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#e7dfbc',
+    },
+    secondary: {
+      main: '#fbfaf8',
+    },
+  }
+});
+
 export default function App() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Nouns, Verbs and the Rest
-          </Typography>
-        </Toolbar>
-      </AppBar>
+    <ThemeProvider theme={theme}>
       <Box sx={{ flexGrow: 1 }}>
-        <InputText />
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Nouns, Verbs and the Rest
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Box sx={{ flexGrow: 1 }}>
+          <InputText />
+        </Box>
       </Box>
-    </Box>
+    </ThemeProvider>
   );
 }
